f2e93ce610feaadf6c72f562403bf12aafacd55f
hiroshi yamato <yamato@gmail.com>
2021-08-07 18:38:55 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,101 @@
-import logo from './logo.svg';
-import './App.css';
+// https://qiita.com/ossan-engineer/items/4757d7457fafd44d2d2f
+// これをやっている。
+
+
+
+import { useState, useEffect } from "react";
+
+function getUser() {
+  return Promise.resolve({ id: '1', name: 'Robin' });
+}
 
 function App() {
+  const [search, setSearch] = useState('');
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const loadUser = async () => {
+      const user = await getUser();
+      setUser(user);
+    };
+
+    loadUser();
+  }, []);
+
+  function handleChange(event) {
+    setSearch(event.target.value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {user ? <p>Singed in as {user.name}</p> : null}
+
+      <Search value={search} onChange={handleChange}>
+        Search:
+      </Search>
+
+      <p>Searches for {search ? search : '...'}</p>
+    </div>
+  );
+}
+
+function Search({ value, onChange, children }) {
+  return (
+    <div>
+      <label htmlFor="search">{children}</label>
+      <input
+        id="search"
+        type="text"
+        value={value}
+        onChange={onChange}
+      />
     </div>
   );
 }
 
 export default App;
+
+
+/*
+import { useState } from "react";
+import axios from 'axios';
+
+const URL = 'http://hn.algolia.com/api/v1/search';
+
+function App() {
+  const [stories, setStories] = useState([]);
+  const [error, setError] = useState(null);
+
+  async function handleFetch(event) {
+    let result;
+
+    try {
+      result = await axios.get(`${URL}?query=React`);
+
+      setStories(result.data.hits);
+    } catch (error) {
+      setError(error);
+    }
+  }
+
+  return (
+    <div>
+      <button type="button" onClick={handleFetch}>
+        Fetch Stories
+      </button>
+
+      {error && <span>Something went wrong ...</span>}
+
+      <ul>
+        {stories.map((story) => (
+          <li key={story.objectId}>
+            <a href={story.url}>{story.title}</a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
+*/
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,63 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import axios from 'axios';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import { render, screen, fireEvent, act, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from './App';
+import Search from './App';
+
+
+let container;
+
+describe('App', () => {
+  test('renders App component', async () => {
+    render(<App />);
+
+    await screen.findByText(/Singed in as/);
+
+    // screen.debug();
+
+    expect(screen.queryByText(/Searches for JavaScript/)).toBeNull();
+
+    await userEvent.type(screen.getByRole('textbox'), 'JavaScript');
+
+    // fireEvent.change(screen.getByRole('textbox'), {
+    //   target: { value: 'JavaScript' },
+    // });
+
+    // screen.debug();
+
+    expect(screen.getByText(/Searches for JavaScript/)).toBeInTheDocument();
+
+    // expect(screen.queryByText(/Signed in as/)).toBeNull(); //nullをアサーションするにはqueryByを使う
+
+    // screen.debug();
+
+    // expect(await screen.findByText(/Singed in as/)).toBeInTheDocument();
+
+    // screen.debug();
+  });
 });
+
+describe('Search', () => {
+  test('calls the onChange callback handler', async () => {
+    const onChange = jest.fn();
+
+    render(
+      <Search value="" onChange={onChange}>
+        Search:
+      </Search>, container
+    );
+
+    await act(async () => await userEvent.type(screen.getByRole('textbox'), 'JavaScript'))
+    // await userEvent.type(screen.getByRole('textbox'), 'JavaScript');
+    await waitFor(() => expect(onChange).toHaveBeenCalledTimes(10));
+    // screen.debug();
+    // console.log(screen.getByRole('textbox'));
+
+    // expect(onChange).toHaveBeenCalledTimes(10);
+  });
+});
+
+
+
+
